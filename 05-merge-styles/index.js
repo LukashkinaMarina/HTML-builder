35405970e7844de2b91c45471fddd0de116f3c4c
LukashkinaMarina <lukaskinamarina1@gmail.com>
2021-11-08 11:57:01 +0300

    feat: update 5 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,49 +1,66 @@
-const fs = require("fs");
-const path = require("path");
-const testFolder = path.join(__dirname, "styles");
-const pathFile = path.join(__dirname, "project-dist/bundle.css");
+const fs = require('fs');
+const path = require('path');
+const styles = path.join(__dirname, 'styles');
+const dist = path.join(__dirname, 'project-dist');
+const bundle = path.join(dist, 'bundle.css');
+
+function createFolder(folder, cb) {
+  fs.access(folder, fs.F_OK, (err) => {
+    if (err) {
+      fs.mkdir(folder, () => {
+        cb && cb();
+      });
+    }
+    cb && cb();
+  });
+}
 
 function streamToString(stream) {
   const chunks = [];
   return new Promise((resolve, reject) => {
-    stream.on("data", (chunk) => chunks.push(Buffer.from(chunk)));
-    stream.on("error", (err) => reject(err));
-    stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
+    stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+    stream.on('error', (err) => reject(err));
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
   });
 }
 
-fs.access(pathFile, fs.F_OK, (err) => {
-  if (err) {
-    fs.writeFile(
-      path.join(__dirname, "project-dist/bundle.css"),
-      "Hello world",
-      (err) => {
-        console.log("Файл был создан");
-      }
-    );
-  }
-});
+function mergeFilesToOneNew(stylesPath, newPath, extType) {
+  fs.readdir(stylesPath, { withFileTypes: true }, (err, files) => {
+    const promise = new Promise((resolve) => {
+      let count = 0;
+      let updated = [];
+      const filtered = files.filter(
+        (file) => file.isFile() && path.extname(file.name) === `.${extType}`
+      );
+      filtered.sort().forEach((file, index) => {
+        fs.stat(path.join(stylesPath, file.name), (e) => {
+          if (e) {
+            console.log(file, `File doesn't exist.`);
+          } else {
+            streamToString(
+              fs.createReadStream(
+                path.join(stylesPath, path.basename(file.name))
+              )
+            ).then((result) => {
+              count++;
 
-fs.readdir(testFolder, { withFileTypes: true }, (err, files) => {
-  files.forEach((file) => {
-    if (file.isFile() && path.extname(file.name) === ".css") {
-      fs.stat(`${testFolder}\\${file.name}`, (err) => {
-        if (err) {
-          console.log(file, `File doesn't exist.`);
-        } else {
-          streamToString(
-            fs.createReadStream(path.join(testFolder, path.basename(file.name)))
-          ).then((result) => {
-            fs.appendFile(
-              path.join(__dirname, "project-dist/bundle.css"),
-              result,
-              (err) => {
-                if (err) throw err;
+              updated[index] = result;
+              if (count === filtered.length) {
+                resolve(updated);
               }
-            );
-          });
-        }
+            });
+          }
+        });
       });
-    }
+    });
+    promise.then((updated) => {
+      fs.writeFile(newPath, updated.join('\n\r'), (e1) => {
+        if (e1) throw e1;
+      });
+    });
   });
+}
+
+createFolder(dist, () => {
+  mergeFilesToOneNew(styles, bundle, 'css');
 });
